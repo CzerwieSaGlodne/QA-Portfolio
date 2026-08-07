dbce74f0cd61f6fbbc108ecf501046665895b4c6
Natalia Świrydow <natalia.swirydow@gmail.com>
2026-08-07 11:55:57 +0200

    add SQL mutations

---
+++ SQL/mutations.sql
@@ -0,0 +1,10 @@
+--Modyfikacja danych--
+
+--Dodawanie nowego klienta: CustomerName = Demo Client, City = Warsaw, Country = Poland--
+INSERT INTO customers(CustomerName, City, Country) VALUES('Demo Client', 'Warsaw', 'Poland');
+
+--Zmiana miasta klienta Demo Client na Kraków.--
+UPDATE customers SET Country ='Kraków' WHERE CustomerName ="Demo Client"
+
+--Usuń klienta Demo Client.--
+DELETE FROM customers WHERE CustomerName ='Demo Client'
